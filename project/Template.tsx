892faa15a78c60d4c85b7b81d56de3dbeb102bc3
Daniele <daniele.vacca81@gmail.com>
2025-06-12 01:25:30 +0200

    Sawsang WIP

--- project/Template.tsx
+++ project/Template.tsx
@@ -16,4 +16,9 @@
       <property name="Name" value="opaopa_shot1" type="string" />
     </properties>
   </tile>
+  <tile id="3">
+    <properties>
+      <property name="Name" value="sawsang" type="string" />
+    </properties>
+  </tile>
 </tileset>
